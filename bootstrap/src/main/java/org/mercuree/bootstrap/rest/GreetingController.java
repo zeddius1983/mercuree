fc575cb9deea5ed0232296227b96c662d125365c
zeddius1983 <avalyugin@gmail.com>
2014-01-19 22:07:20 +0400

    Sample restful controller added.

---
+++ bootstrap/src/main/java/org/mercuree/bootstrap/rest/GreetingController.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mercuree.bootstrap.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * TODO: javadoc
+ * <p/>
+ *
+ * @author Alexander Valyugin
+ */
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    @ResponseBody
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return "Hello, " + name;
+    }
+
+}
